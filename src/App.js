bb80289af9b4e5daf11aabc9e8f2f2e0a16f3186
Christopher Patty <christopher.patty@aggiemail.usu.edu>
2018-10-31 17:52:04 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,69 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useReducer, useEffect } from 'react';
+import 'normalize.css'
 import './App.css';
+import CreateBox from './components/CreateBox'
+import TodoList from './components/TodoList'
+import shortid from 'shortid'
+import ls from 'local-storage'
+import styled from 'react-emotion'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+export const TodosContext = React.createContext([])
+
+const setTodoKeyByIndex = ({todos, key, value, index}) => ([
+  ...todos.slice(0, index),
+  {...todos[index], [key]: value},
+  ...todos.slice(index + 1)
+])
+
+const App = () => {
+  const [todos, dispatch] = useReducer(
+    (todos, action) => {
+      switch (action.type) {
+        case 'ADD_TODO':
+          return [{done: false, text: action.todo, id: shortid.generate()}, ...todos]
+        case 'SET_DONE':
+          return setTodoKeyByIndex({todos, key: 'done', value: action.done, index: action.index })
+                .sort((a,b) => {
+                  if(a.done) return 1;
+                  if(b.done) return -1;
+                  return 0;
+                })
+        case 'SET_TEXT':
+          return setTodoKeyByIndex({todos, key: 'text', value: action.text, index: action.index})
+        case 'DELETE_TODO':
+          return [
+            ...todos.slice(0, action.index),
+            ...todos.slice(action.index + 1)
+          ]
+        default:
+          return todos;
+      }
+    }, ls.get("TODOS") || []
+  )
+  useEffect(() => {
+    ls.set("TODOS", todos)
+  }, [todos])
+
+  const handleSubmit = todo => {
+    dispatch({
+      type: 'ADD_TODO',
+      todo
+    })
   }
+
+  return (
+    <div className="App">
+      <TodosContext.Provider value={{todos, dispatch}}>
+        <Title>React Hooks Todo</Title>
+        <CreateBox onSubmit={handleSubmit}/>
+        <TodoList todos={todos}/>
+      </TodosContext.Provider>
+    </div>
+  );
 }
+
+const Title = styled('h1')({
+  textAlign: 'center'
+})
 
 export default App;
